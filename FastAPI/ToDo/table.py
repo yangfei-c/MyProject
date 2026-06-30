04d6d9dd21a893cf55a1d78d3edeb3952937b285
YangFei <3260693417@qq.com>
2026-06-30 20:16:33 +0800

    引入用户，解耦数据库连接

---
+++ FastAPI/ToDo/table.py
@@ -0,0 +1,19 @@
+from sqlalchemy import Column, String, Integer, Boolean, ForeignKey
+from sqlalchemy.orm import declarative_base, relationship
+
+Base = declarative_base()
+class User(Base):
+    __tablename__ = "users"
+    id = Column(Integer, primary_key=True, index=True)
+    username = Column(String, unique=True,index=True)
+    hash_password = Column(String)
+    todos=relationship("DBToDo", back_populates="owner")
+
+class DBToDo(Base):
+    __tablename__ = "todos"
+    id = Column(Integer, primary_key=True, index=True)
+    title = Column(String, index=True)
+    completed = Column(Boolean, default=False)
+
+    owner_id = Column(Integer, ForeignKey("users.id"))
+    owner = relationship("User", back_populates="todos")
